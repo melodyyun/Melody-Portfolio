f47c0d2f80c705935b2fc2743e9e978abc6a14cd
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-31 17:38:02 -0400

    added images

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('scripts', () => {
 
 gulp.task('watch', function () {
     gulp.watch('./dev/scripts/*.js', ['scripts']);
-    gulp.watch('./dev/styles/*.scss', ['styles']);
+    gulp.watch('dev/**/*.scss', ['styles']);
     gulp.watch('./dev/styles/components/*.scss', ['styles']);
     gulp.watch('*.html', reload);
 });
